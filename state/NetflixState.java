2c3d4ac4ffce25e4e5cb09974d06fa350c05fc7b
Cody616 <89538011+Cody616@users.noreply.github.com>
2022-11-20 22:41:54 -0500

    Update NetflixState.java

--- state/NetflixState.java
+++ state/NetflixState.java
@@ -1,49 +1,64 @@
-package state;
-
-import java.util.*;
-
-public class NetflixState implements State {
-
+/*
+ * @author CodyTang
+ */
+public class NetflixState implements State 
+{
     private TV tv;
-    private String[] movies = new String[] {"Peppa Pig","My Little Pony", "Garfield", "Teenage Mutatn Ninja Turtles"};
-    private String[] tvShows = new String[] {"The Land Before Time", "Frozen", "The Little Mermaid", "Ice Age"};
+    private String[] tvShows = new String[] {"Peppa Pig","My Little Pony", "Garfield", "Teenage Mutatn Ninja Turtles"};
+    private String[] movies = new String[] {"The Land Before Time", "Frozen", "The Little Mermaid", "Ice Age"};
 
-    public String presssHomeButton() 
+    public NetflixState(TV tv)
     {
+        this.tv = tv;
+    }
+    /*
+     * sets to home state
+     */
+    public String pressHomeButton() {
+        tv.setState(tv.getHomeState());
         return "\nLoading the Home Screen...";
     }
-
+    /*
+     * presses netlix state
+     */
     public String pressNetflixButton() 
     {
         return "\nYou are already on Netflix Screen";
     }
-
+    /*
+     * presses hulu state
+     */
     public String pressHuluButton() 
     {
         tv.setState(tv.getHuluState());
         return "\nLoading Hulu...";
-        
     }
-
+    /*
+     * returns netflix moveis
+     */
     public String pressMovieButton() 
     {
-        String move[] = {"The Land Before Time", "Frozen", "The Little Mermaid", "Ice Age"};
-        System.out.println("Netflix Moveis: ");
-        for(int i = 0; i<movies.length; i++)
+        String movies = "";
+        System.out.println("\nNetflix Movies: ");
+        for(int i = 0; i< this.movies.length; i++)
         {
-            System.out.println(movies[i]);
+            movies += "- " + this.movies[i] + "\n";
         }
-        return "";
+        return movies;
     }
-
+    /*
+     * returns netflix shows
+     */
     public String pressTVButton() 
     {
-        String shows[] = {"Peppa Pig","My Little Pony", "Garfield", "Teenage Mutatn Ninja Turtles"};
-        System.out.println("Netflix TV Shows: ");
-        for(int i = 0; i<tvShows.length; i++)
+        String shows = "";
+        System.out.println("\nNetflix TV Shows: ");
+        for(int i = 0; i< this.tvShows.length; i++)
         {
-            System.out.println(tvShows[i]);
+            shows += "- " + this.tvShows[i] + "\n";
         }
-        return "";
+        return shows;
     }
+
+    
 }
